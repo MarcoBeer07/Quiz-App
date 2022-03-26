e51dfba8bad28f4967c24777592714c51d8b0a60
Marco <marco.beer93@web.de>
2022-03-26 14:35:39 +0100

    added question array

---
+++ script.js
@@ -0,0 +1,45 @@
+let questionsHTML = [{
+        "question": "Wer hat HTML erfunden?",
+        "answer1": "Robbie Williams",
+        "answer2": "Lady Gaga",
+        "answer3": "Tim Berners-Lee",
+        "answer4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Mit welchem Attribut erstellt man eine Unsortierte Liste ?",
+        "answer1": "<a>",
+        "answer2": "<ul>",
+        "answer3": "<img>",
+        "answer4": "<table>",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet HTML ? ",
+        "answer1": "Hypertext Markup Language",
+        "answer2": "Hybrid Text Module Loading",
+        "answer3": "Hypertext Module Large",
+        "answer4": "Hyper Toggle Module Language",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie fügt man eine JS Variable in HTML ein ? ",
+        "answer1": "<var>variable</var>",
+        "answer2": "<${variable}>",
+        "answer3": "<a>variable</a>",
+        "answer4": "<table>variable</tabl>",
+        "right_answer": 2
+    }
+
+];
+
+
+
+
+
+
+function init() {
+
+
+
+}
