b35c48165fd23641e8d438cbf7c7de95b782a8e8
Marco <marco.beer93@web.de>
2022-03-27 12:39:04 +0200

    added question positions

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-let questionsHTML = [{
+let questions = [{
         "question": "Wer hat HTML erfunden?",
-        "answer1": "Robbie Williams",
-        "answer2": "Lady Gaga",
-        "answer3": "Tim Berners-Lee",
-        "answer4": "Justin Bieber",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
         "right_answer": 3
     },
     {
         "question": "Mit welchem Attribut erstellt man eine Unsortierte Liste ?",
-        "answer1": "<a>",
-        "answer2": "<ul>",
-        "answer3": "<img>",
-        "answer4": "<table>",
+        "answer_1": "<a>",
+        "answer_2": "<ul>",
+        "answer_3": "<img>",
+        "answer_4": "<table>",
         "right_answer": 2
     },
     {
         "question": "Was bedeutet HTML ? ",
-        "answer1": "Hypertext Markup Language",
-        "answer2": "Hybrid Text Module Loading",
-        "answer3": "Hypertext Module Large",
-        "answer4": "Hyper Toggle Module Language",
+        "answer_1": "Hypertext Markup Language",
+        "answer_2": "Hybrid Text Module Loading",
+        "answer_3": "Hypertext Module Large",
+        "answer_4": "Hyper Toggle Module Language",
         "right_answer": 1
     },
     {
         "question": "Wie fügt man eine JS Variable in HTML ein ? ",
-        "answer1": "<var>variable</var>",
-        "answer2": "<${variable}>",
-        "answer3": "<a>variable</a>",
-        "answer4": "<table>variable</tabl>",
+        "answer_1": "<var>variable</var>",
+        "answer_2": "<${variable}>",
+        "answer_3": "<a>variable</a>",
+        "answer_4": "<table>variable</tabl>",
         "right_answer": 2
     }
 
@@ -35,11 +35,22 @@ let questionsHTML = [{
 
 
 
-
+let currentQuestion = 0;
 
 
 function init() {
 
+    showQuestion()
+
+}
 
 
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
